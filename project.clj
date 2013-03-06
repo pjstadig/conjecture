40a7304c650890d975b0bfdaabcf710c9cd253a2
Paul Stadig <paul@stadig.name>
2013-03-06 16:38:34 -0500

    Adds Eclipse license to the repo, and fixes up source headers.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject name.stadig/conjecture "0.1.0"
   :description "A Clojure testing library forked from clojure.test."
   :url "http://github.com/pjstadig/conjecture"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]])
