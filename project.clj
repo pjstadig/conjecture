47aa3510d5507afe6cc3afef63ea3954725d03af
Paul Stadig <paul@stadig.name>
2013-03-06 16:48:41 -0500

    Adds lein-conjecture plugin and defines "test" alias.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/pjstadig/conjecture"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :plugins [[name.stadig/lein-conjecture "0.1.0"]]
+  :aliases {"test" "conjecture"})
