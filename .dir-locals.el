e59db2c59b7356c2765331276b0c209ce5ab5430
Paul Stadig <paul@stadig.name>
2013-03-06 10:36:32 -0500

    Initial import of clojure.test.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((fill-column . 80)
+         (whitespace-line-column . nil)
+         (eval ignore-errors
+               (require 'whitespace)
+               (whitespace-mode t))))
+ (clojure-mode . ((clojure-test-ns-segment-position . 1))))
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject name.stadig/clojure-test "0.1.0-SNAPSHOT"
+  :description "clojure.test but not in clojure.jar"
+  :url "http://github.com/pjstadig/clojure-test"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]])
