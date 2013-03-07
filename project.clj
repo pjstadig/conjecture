77a617ea81b3da7ed39c0b4206d4ef2b46fd786d
Paul Stadig <paul@stadig.name>
2013-03-07 05:16:23 -0500

    Updates to documentation.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject name.stadig/conjecture "0.1.0"
-  :description "A Clojure testing library forked from clojure.test."
+  :description "A ~clojure.test~ compatible third-party testing library for
+  Clojure."
   :url "http://github.com/pjstadig/conjecture"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
