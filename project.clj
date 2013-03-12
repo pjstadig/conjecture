11cd2d1498cd4fb65c0c06b42237943388db2d57
Paul Stadig <paul@stadig.name>
2013-03-11 20:02:52 -0400

    Bump to 0.3.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject conjecture "0.3.0-SNAPSHOT"
+(defproject conjecture "0.3.0"
   :description "A clojure.test compatible third-party testing library for
   Clojure."
   :url "http://github.com/pjstadig/conjecture"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :plugins [[lein-conjecture "0.2.0-SNAPSHOT"]]
+  :plugins [[lein-conjecture "0.2.0"]]
   :aliases {"test" "conjecture"})
